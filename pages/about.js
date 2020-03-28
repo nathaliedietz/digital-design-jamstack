2fc3b0b3077051a6812ee9ce9a6bc15e00555626
i051575 <nathalie.dietz@sap.com>
2020-03-28 23:48:14 +0100

    Adding about page

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+
+
+import Layout from "../components/Layout"
+
+const About = () => (
+    <Layout>
+   <div>
+        <h1>About me</h1>
+    </div>
+    </Layout>
+ 
+
+
+);
+
+export default About;
